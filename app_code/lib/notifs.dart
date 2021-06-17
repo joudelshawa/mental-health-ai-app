0ee1b7d120f887a62c54bd0bbda797948a02988b
mbattad <35310158+mbattad@users.noreply.github.com>
2021-06-17 12:47:41 -0500

    cleaned up notifs.dart
    removed unused notification methods

--- app_code/lib/notifs.dart
+++ app_code/lib/notifs.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
+import 'main.dart';
+
 class NotificationService extends ChangeNotifier {
   final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin =
   FlutterLocalNotificationsPlugin();
@@ -22,13 +24,19 @@ class NotificationService extends ChangeNotifier {
         android: androidInitializationSettings,
         iOS: iosInitializationSettings);
 
-    await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+          onSelectNotification: _onSelectNotification);
+  }
+
+  Future<void> _onSelectNotification(String? payload) async {
+    runApp(FromNotifs());
   }
 
   //Instant Notifications
   instantNotification() async {
     var android = AndroidNotificationDetails("id", "channel", "description",
-        priority: Priority.high,importance: Importance.max,);
+        priority: Priority.high,
+        importance: Importance.max);
 
     var ios = IOSNotificationDetails();
 
@@ -37,67 +45,6 @@ class NotificationService extends ChangeNotifier {
     await _flutterLocalNotificationsPlugin.show(
         0, "Did you just close Twitter?", "Tell us about it!", platform,
         payload: "item");
-  }
-
-  //Image notification
-  Future imageNotification() async {
-    var bigPicture = BigPictureStyleInformation(
-        DrawableResourceAndroidBitmap("ic_launcher"),
-        largeIcon: DrawableResourceAndroidBitmap("ic_launcher"),
-        contentTitle: "Demo image notification",
-        summaryText: "This is some text",
-        htmlFormatContent: true,
-        htmlFormatContentTitle: true);
-
-    var android = AndroidNotificationDetails("id", "channel", "description",
-        styleInformation: bigPicture);
-
-    var platform = new NotificationDetails(android: android);
-
-    await _flutterLocalNotificationsPlugin.show(
-        0, "Demo Image notification", "Tap to do something", platform,
-        payload: "Welcome to demo app");
-  }
-
-  //Stylish Notification
-  Future stylishNotification() async {
-    var android = AndroidNotificationDetails("id", "channel", "description",
-        color: Colors.deepOrange,
-        enableLights: true,
-        enableVibration: true,
-        largeIcon: DrawableResourceAndroidBitmap("ic_launcher"),
-        styleInformation: MediaStyleInformation(
-            htmlFormatContent: true, htmlFormatTitle: true));
-
-    var platform = new NotificationDetails(android: android);
-
-    await _flutterLocalNotificationsPlugin.show(
-        0, "Demo Stylish notification", "Tap to do something", platform);
-  }
-
-  //Sheduled Notification
-
-  Future sheduledNotification() async {
-    var interval = RepeatInterval.everyMinute;
-    var bigPicture = BigPictureStyleInformation(
-        DrawableResourceAndroidBitmap("ic_launcher"),
-        largeIcon: DrawableResourceAndroidBitmap("ic_launcher"),
-        contentTitle: "Demo image notification",
-        summaryText: "This is some text",
-        htmlFormatContent: true,
-        htmlFormatContentTitle: true);
-
-    var android = AndroidNotificationDetails("id", "channel", "description",
-        styleInformation: bigPicture);
-
-    var platform = new NotificationDetails(android: android);
-
-    await _flutterLocalNotificationsPlugin.periodicallyShow(
-        0,
-        "Demo Sheduled notification",
-        "Tap to do something",
-        interval,
-        platform);
   }
 
   //Cancel notification
