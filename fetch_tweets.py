7ae84c650316fc9d710fe19b04475599cec6c068
e-lee <emilylee.email@gmail.com>
2021-06-21 00:55:56 -0700

    cleaned codes

--- fetch_tweets.py
+++ fetch_tweets.py
@@ -1,23 +1,35 @@
-import sys, tweepy
+"""puts tweets in CSV file"""
+
+import sys
+import tweepy
 import csv
-'''' authentication function '''
+
+NUM_TWEETS = 200  # change number in brackets for more tweets
+
+
 def twitter_auth():
+    """ authentication function """
     try:
+        # application tokens needed to use twitter API
         consumer_key = 'vTJwEoiaNx0RpHHlfRjSfKMwI'
         consumer_secret = 'ovChqGW35Hdken31HoKIFr6DPRLmL9aaJnEl9O9irXLUB9tQFM'
+        # tokens that allow API requests on your account's behalf
         access_token = '1014489193-9D7oZPGOvmkTfmcmb6MbIhYts2ES1cCjTtdZFXl'
         access_secret = 'LK2e7qU4gI6hin7XU1kVyhtBY96ARRyUQdLp8Gs7iX9YT'
     except KeyError:
         sys.stderr.write("TWITTER_* environment variable not set\n")
         sys.exit(1)
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token,access_secret)
+    auth.set_access_token(access_token, access_secret)
     return auth
 
+
 def get_twitter_client():
+    """gets twitter client using given authentication"""
     auth = twitter_auth()
-    client = tweepy.API(auth,wait_on_rate_limit=True)
-    return client
+    new_client = tweepy.API(auth, wait_on_rate_limit=True)
+    return new_client
+
 
 if __name__ == '__main__':
     # user = input("Enter username: ")
@@ -26,6 +38,6 @@ if __name__ == '__main__':
     with open('data_new.csv', 'w', newline='', encoding='utf-8') as f:
         writer = csv.writer(f, delimiter=',')
         writer.writerow(['Tweet'])
-        for status in tweepy.Cursor(client.home_timeline, screen_name=user).items(200): # change number in brackets for more tweets
+        for status in tweepy.Cursor(client.home_timeline, screen_name=user).items(NUM_TWEETS):
             print(status.text, '\n')
-            writer.writerow([status.text]) # puts data in a csv file
+            writer.writerow([status.text])  # puts data in a csv file
--- ml_news_training.py
+++ ml_news_training.py
@@ -1,3 +1,7 @@
+"""trains models
+saves vectorizer, encoder, selector... etc.
+i.e. trains on the 40 000 labelled tweets"""
+
 # machine learning model on news category dataset
 import copy
 import numpy as np
@@ -5,7 +9,6 @@ import matplotlib.pyplot as plt
 import re
 import nltk
 nltk.download('stopwords')
-from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.preprocessing import LabelEncoder
 from sklearn.feature_selection import VarianceThreshold
@@ -15,84 +18,72 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.ensemble import RandomForestClassifier
-#from sklearn.metrics import accuracy_score
-#from sklearn.model_selection import cross_val_score, KFold
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
+#from sklearn.metrics import accuracy_score
+#from sklearn.model_selection import cross_val_score, KFold
+from sklearn.model_selection import train_test_split
 import seaborn as sns
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import pickle
 
-subset = pd.read_csv('sample_1000_shuffled.csv',lineterminator='\n')
+VAR_THRESH = 0.001
+N_EST = 40
 
-# tweets = subset['Tweet Preprocessed'].values.tolist()
-# categories = subset['Category'].values.tolist()
+subset = pd.read_csv('sample_1000_shuffled.csv', lineterminator='\n')  # import training data as pd dataframe
 
-res = []
+# extract sorted csv data
+categories = subset['Category'].values.astype('U').tolist()
+tweets = subset['Tweet Preprocessed\r'].values.astype('U').tolist()
 
-for column in subset.columns:
-    # Storing the rows of a column
-    # into a temporary list
-    subset[column] = subset[column].values.astype('U')
-    li = subset[column].tolist()
-    # appending the temporary list
-    res.append(li)
-tweets = res[1]
-categories = res[0]
-
-# print(res)
-
-# print(tweets)
-# print(categories)
-
-# '''splitting data in 3 parts'''
+'''splitting data into test and train parts'''
+# set random_state = 0 to ensure a deterministic split of data
+# perform twice to get split training set into test/development
 title_tr, title_te, category_tr, category_te = train_test_split(tweets, categories, random_state=0)
 title_tr, title_de, category_tr, category_de = train_test_split(title_tr, category_tr, random_state=0)
-print("Training: ", len(title_tr))
-print("Development: ", len(title_de),)
-print("Testing: ", len(title_te))
+print("Training: ", len(title_tr), "\nDevelopment: ", len(title_de), "\nTesting: ", len(title_te))
 
-
-# "vectorization of data using bag of words"
-tokenizer = nltk.tokenize.RegexpTokenizer(r"\w+")
-stop_words = nltk.corpus.stopwords.words("english")
+'''Vectorize data using bag of words'''
+tokenizer = nltk.tokenize.RegexpTokenizer(r"\w+")  # form tokenizer out of alphabetic sequences
+stop_words = nltk.corpus.stopwords.words("english")  # get common words (stopwords) to filter them out of tokenization
 vectorizer = CountVectorizer(tokenizer=tokenizer.tokenize, stop_words=stop_words)
 
-# title_tr = title_tr
-# x = v.fit_transform(df['Review'].values.astype('U'))  ## Even astype(str) would work
-vectorizer.fit(iter(title_tr))
-Xtr = vectorizer.transform(iter(title_tr))
-Xde = vectorizer.transform(iter(title_de))
-Xte = vectorizer.transform(iter(title_te))
+# apply the tokenizer to each tweet
+# vectorizer.fit(iter(title_tr))  # todo: is the iter object really necessary? i don't think it changes anything?
+Xtr = vectorizer.fit_transform(title_tr)
+Xde = vectorizer.transform(title_de)
+Xte = vectorizer.transform(title_te)
 
+'''Encode labelled categories into numerical values'''
 encoder = LabelEncoder()
-encoder.fit(category_tr)
-Ytr = encoder.transform(category_tr)
+Ytr = encoder.fit_transform(category_tr)
 Yde = encoder.transform(category_de)
 Yte = encoder.transform(category_te)
 
-''''feature reduction !!!'''
-print("Number of features before reduction : ", Xtr.shape[1], Xde.shape[1], Xte.shape[1])
-selection = VarianceThreshold(threshold=0.001)
+'''Save copies of vectorized/encoded training data'''
 Xtr_whole = copy.deepcopy(Xtr)
 Ytr_whole = copy.deepcopy(Ytr)
-selection.fit(Xtr)
-Xtr = selection.transform(Xtr)
+
+''''feature reduction !!!'''
+print("Number of features before reduction : ", Xtr.shape[1], Xde.shape[1], Xte.shape[1])
+selection = VarianceThreshold(threshold=VAR_THRESH)
+Xtr = selection.fit_transform(Xtr)
 Xde = selection.transform(Xde)
 Xte = selection.transform(Xte)
 print("Number of features after reduction : ", Xtr.shape[1], Xde.shape[1], Xte.shape[1])
 
-'''''sampling data'''
+'''''Sampling data categories'''
 # count number of diff labels in dataset and plot
-labels = list(set(Ytr))
+labels = list(set(Ytr))  # use a set then list to extract unique elements
 counts = []
 for label in labels:
     counts.append(np.count_nonzero(Ytr == label))
 plt.pie(counts, labels=labels, autopct='%1.1f%%')
-# plt.show()
+plt.show()
 
-''''data not uniformly distributed so oversampling!!'''
+''''data not uniformly distributed so oversampling'''
+# synthesize new examples to make up for imbalanced classification data
 sm = SMOTE(random_state=42)
 Xtr, Ytr = sm.fit_resample(Xtr, Ytr)
 labels = list(set(Ytr))
@@ -100,22 +91,24 @@ counts = []
 for label in labels:
     counts.append(np.count_nonzero(Ytr == label))
 plt.pie(counts, labels=labels, autopct='%1.1f%%')
-# plt.show()
+plt.show()
 
 ''''training models!!!'''
 highest_prec_model = ''
 highest_prec_val = 0
-
+best_pred = []
 
 # baseline model (dummy stratified classifier)
+# gives performance as good as "guessing"
 print("dummy classifier - stratified")
 dc = DummyClassifier(strategy="stratified")
 dc.fit(Xtr, Ytr)
 pred = dc.predict(Xde)
 print(classification_report(Yde, pred, target_names=encoder.classes_))
-if (dc.score(Xde, Yde) > highest_prec_val):
-    highest_prec_val = dc.score(Xde,Yde)
+if dc.score(Xde, Yde) > highest_prec_val:
+    highest_prec_val = dc.score(Xde, Yde)
     highest_prec_model = 'dummy classifier'
+    best_pred = pred
 
 # decision tree
 print("decision tree")
@@ -123,19 +116,21 @@ dt = DecisionTreeClassifier(random_state=5)
 dt.fit(Xtr, Ytr)
 pred = dt.predict(Xde)
 print(classification_report(Yde, pred, target_names=encoder.classes_))
-if (dt.score(Xde, Yde) > highest_prec_val):
-    highest_prec_val = dt.score(Xde,Yde)
+if dt.score(Xde, Yde) > highest_prec_val:
+    highest_prec_val = dt.score(Xde, Yde)
     highest_prec_model = 'decision tree'
+    best_pred = pred
 
 # random forest
 print("random forest")
-rf = RandomForestClassifier(n_estimators=40)
+rf = RandomForestClassifier(n_estimators=N_EST)
 rf.fit(Xtr, Ytr)
 pred = rf.predict(Xde)
 print(classification_report(Yde, pred, target_names=encoder.classes_))
-if (rf.score(Xde, Yde) > highest_prec_val):
+if rf.score(Xde, Yde) > highest_prec_val:
     highest_prec_val = rf.score(Xde, Yde)
     highest_prec_model = 'random forest'
+    best_pred = pred
 
 # multinomial naive bayesian
 print("multinomial naive bayesian")
@@ -143,9 +138,10 @@ nb = MultinomialNB()
 nb.fit(Xtr, Ytr)
 pred = nb.predict(Xde)
 print(classification_report(Yde, pred, target_names=encoder.classes_))
-if (nb.score(Xde, Yde) > highest_prec_val):
+if nb.score(Xde, Yde) > highest_prec_val:
     highest_prec_val = nb.score(Xde, Yde)
     highest_prec_model = 'naive bayes'
+    best_pred = pred
 
 # support vector classification
 print("support vector classification")
@@ -154,9 +150,10 @@ svc = SVC()
 svc.fit(Xtr, Ytr)
 pred = svc.predict(Xde)
 print(classification_report(Yde, pred, target_names=encoder.classes_))
-if (svc.score(Xde, Yde) > highest_prec_val):
+if svc.score(Xde, Yde) > highest_prec_val:
     highest_prec_val = svc.score(Xde, Yde)
     highest_prec_model = 'svc'
+    best_pred = pred
 
 # multilayered perceptron
 print("multilayered perceptron")
@@ -164,22 +161,22 @@ mlp = MLPClassifier(solver='adam', alpha=1e-5, hidden_layer_sizes=(100, 20), ran
 mlp.fit(Xtr, Ytr)
 pred = mlp.predict(Xde)
 print(classification_report(Yde, pred, target_names=encoder.classes_))
-if (mlp.score(Xde,Yde) > highest_prec_val):
+if mlp.score(Xde,Yde) > highest_prec_val:
     highest_prec_val = mlp.score(Xde, Yde)
     highest_prec_model = 'multilayered perceptron'
+    best_pred = pred
 
 ''''ADD BEST MODEL DEPENDING ON RESULTS'''
-#maybe multinomial naive bayes??
+# multinomial naive bayes is best at the moment
 '''predicting test data'''
 pred = nb.predict(Xte)
 print(classification_report(Yte, pred, target_names=encoder.classes_))
 sns.heatmap(confusion_matrix(Yte, pred))
 
-
 '''why multinomial naive bayes is good'''
 nb1 = MultinomialNB()
 nb1.fit(Xtr_whole, Ytr_whole)
-coefs = nb1.coef_
+coefs = nb1.coef_  # todo: replace with feature_log_prob_?
 target_names = encoder.classes_
 
 # added this part idk
@@ -189,16 +186,19 @@ vocabulary = vectorizer.vocabulary_
 for word in vocabulary:
     index = vocabulary[word]
     reverse_vocabulary[index] = word
-#
 
 for i in range(len(target_names)):
     words = []
     for j in coefs[i].argsort()[-20:]:
         words.append(reverse_vocabulary[j])
-    print (target_names[i], '-', words, "\n")
+    print(target_names[i], '-', words, "\n")
 # should print out all the coefficents of the features
 # and then print the top 20 words based on its weight.
 
+'''Print categories of test set (assume random forest is best)'''
+print("Test Set Predicted Labels: ")
+for num_label, label in enumerate(labels):
+    print(str((encoder.inverse_transform(labels)[num_label]) + r": " + str(best_pred.tolist().count(label))))
 
 print("final results")
 print(highest_prec_val, "with", highest_prec_model)
